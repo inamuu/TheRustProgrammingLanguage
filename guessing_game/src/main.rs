1ef4b8d112657389a6a0187e3cf62e835ce0a919
inamuu <8310973+inamuu@users.noreply.github.com>
2022-02-20 16:32:33 +0900

    add check type

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -17,11 +17,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Faild to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type number!");
+
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!");
-        Ordering::Greater => println!("Too big!");
-        Ordering::Equal => println!("You win!");
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
     }
 }
