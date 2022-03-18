36c57e6323c624d3e253ad58b086f79aaf319fe3
inamuu <8310973+inamuu@users.noreply.github.com>
2022-03-18 11:24:31 +0900

    rectangles

---
+++ rectangles/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        //長方形の面積は{}平方ピクセルです
+        "The aread of th rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width + height
+}
